c5154b8aa4020d0d93275c985d6b2daf83a5bea2
marnielcorna <83858563+marnielcorna@users.noreply.github.com>
2021-12-08 10:43:59 +0100

    Cypress Testing

---
+++ cypress/integration/todomvc-actions.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+import { TodoPage } from "../page-objects/todo-page"
+
+describe('todo actions', ()=>{
+    const todoPage = new TodoPage();
+    beforeEach(()=>{
+        todoPage.navigate()
+        todoPage.addTodo('Clean room')
+    })
+it('should add a new todo to the list',()=>{
+    
+    todoPage.validateTodoTxt(0, 'Clean room')
+    cy.get('.toggle').should('not.be.checked')
+})
+
+it('should mark a todo as completed',()=>{
+
+cy.get('.toggle').click()
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+})
+
+it('should complete todos',()=>{
+    cy.get('.toggle').click()
+    cy.contains('Clear').click()
+    cy.get('.todo-list').should('not.have.descendants', 'li')
+})
+})
